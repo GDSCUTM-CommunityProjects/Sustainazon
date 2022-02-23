c11c336a9a1cbbb40f4075b77ec8ae3ab89e52e5
Shubh Bapna <shubh.bapna@mail.utoronto.ca>
2022-02-23 14:50:23 -0500

    added server boilerplate code

---
+++ server/server.js
@@ -0,0 +1,24 @@
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+// starting the express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+// parse cookies and body and enable cors
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: "GET,POST,DELETE,PATCH",
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
